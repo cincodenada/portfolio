4a606b7ec38dca2d3a3020ec484f109c96ef755e
Joel Bradshaw <cincodenada@gmail.com>
2016-10-05 01:57:43 -0700

    Some quick fixes to the project page
    It's not super pretty, but it's better, which is good enough for now

--- projects/projectpage.php
+++ projects/projectpage.php
@@ -1,14 +1,19 @@
 <?php
 include('../vendor/autoload.php');
 use \Michelf\Markdown;
+$alldata = yaml_parse_file("projects.yaml");
 $pagename = $_GET['page'];
-$pagedata = yaml_parse_file("$pagename.yaml");
+$pagedata = $alldata[$pagename];
 ?>
 <?php include('../inc/header.php') ?>
-<div class="sidebar">
-<h1><a href="<?=$pagedata['link']?>"><?=$pagedata['title']?></a></h1>
+<div role="main" class="project_page">
 <img class="articleimg" src="/img/<?=$pagename?>.png"/>
-<?=Markdown::defaultTransform(file_get_contents("$pagename.md")) ?>
+<h1><a href="<?=$pagedata['link']?>"><?=$pagedata['title']?></a></h1>
+<?php if(file_exists("$pagename.md")): ?>
+  <?=Markdown::defaultTransform(file_get_contents("$pagename.md")) ?>
+<?php else: ?>
+  <?=$pagedata['summary']?>
+<?php endif ?>
 <?php include('../inc/projects_footer.php') ?>
 </div>
 <?php include('../inc/footer.php') ?>
